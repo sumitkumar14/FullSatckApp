e724c5c10df3c75eef9655a5dcc2f0923bc9f40e
Sumit Kumar <sumitkumar14695@gmail.com>
2024-07-21 18:29:17 +0530

    code refactoring

--- frontEnd/vue3-app/src/views/HomeView.vue
+++ frontEnd/vue3-app/src/views/HomeView.vue
@@ -1,56 +1,45 @@
 <template>
-  <div id="app">
-    <D3Tree :data="treeData" />
-    <!-- <HierarchyView/> -->
+  <div>
+    <HierarchyChart v-if="!loading && !error" :data="treeData.items[0]" />
+    <div v-else-if="loading">Loading...</div>
+    <div v-else-if="error">Error: {{ error.message }}</div>
   </div>
 </template>
 
 <script>
-import D3Tree from '@/components/HierarchyChart.vue';
-// import  HierarchyView from '@/components/HierarchyView.vue';
+import { useQuery } from '@vue/apollo-composable';
+import gql from "graphql-tag"
+import HierarchyChart from '@/components/HierarchyChart.vue';
+
+const GET_HIERARCHY = gql`
+  query GetHierarchy {
+    items {
+      name
+      description
+      children {
+        name
+        description
+        children {
+          name
+          description
+        }
+      }
+    }
+  }
+`;
 
 export default {
-  name: 'App',
+  name: 'HomeView',
   components: {
-    D3Tree,
-    // HierarchyView,
+    HierarchyChart
   },
-  data() {
+  setup() {
+    const { result, loading, error } = useQuery(GET_HIERARCHY);
+
     return {
-      treeData: {
-        name: "A",
-        description: "This is a description of A",
-        children: [
-          {
-            name: "B",
-            description: "This is a description of B",
-            children: [
-              {
-                name: "B-1",
-                description: "This is a description of B-1"
-              },
-              {
-                name: "B-2",
-                description: "This is a description of B-2"
-              },
-              {
-                name: "B-3",
-                description: "This is a description of B-3"
-              }
-            ]
-          },
-          {
-            name: "C",
-            description: "This is a description of C",
-            children: []
-          },
-          {
-            name: "D",
-            description: "This is a description of D",
-            children: []
-          }
-        ]
-      }
+      treeData: result,
+      loading,
+      error
     };
   }
 };
